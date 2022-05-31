09d3bace52f33a846ff47e91e5509c4a3877d159
Khaled-Waled <khaledw.hamed001@gmail.com>
2022-05-31 21:01:38 +0200

    Completed sendRequest()

--- app/src/main/java/com/example/android_communicator/MainActivity.kt
+++ app/src/main/java/com/example/android_communicator/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.android_communicator
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.android_communicator.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
@@ -22,10 +32,30 @@ class MainActivity : AppCompatActivity() {
             sendRequest(request)
         }
     }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isDeviceOnline(context: Context):Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        if(network == null)
+            return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network)
+        if(activeNetwork == null)
+            return false
+
+        return (activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+
+    }
     private fun constructRequest(): MyRequest {
         var myRequest = MyRequest()
         myRequest.url = binding.URLEditText.text.toString()
-        myRequest.type = "POST"
+        myRequest.type = when (binding.requestTypeGroup.checkedRadioButtonId)
+        {
+            R.id.option_get ->  "GET"
+            R.id.option_post -> "POST"
+            else -> "GET"
+        }
         myRequest.timeOut = 10000 //= 10 seconds
         myRequest.body = binding.requestBodyInput.text.toString()
         myRequest.headers = binding.headersEditText.text.toString().split(";") as MutableList<String>
@@ -33,6 +63,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendRequest(myRequest: MyRequest){
+        Toast.makeText(applicationContext,"this is NOT a toast message",Toast.LENGTH_SHORT).show()
         val url = URL(myRequest.url)
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = myRequest.type
@@ -43,7 +74,30 @@ class MainActivity : AppCompatActivity() {
         connection.setRequestProperty("Content-length", data.size.toString())
         connection.setRequestProperty("Content-Type", "application/json")
 
-        TODO("send the request")
+        //Write body
+        try{
+            val outputStream = DataOutputStream(connection.outputStream)
+            outputStream.write(data)
+        }
+        catch(exception: Exception){
+            Toast.makeText(applicationContext,"Connection error...",Toast.LENGTH_SHORT).show()
+        }
+
+        val inputStream:DataInputStream
+        if(connection.responseCode/100 == 2){
+            inputStream = DataInputStream(connection.inputStream)
+        }
+        else{
+            inputStream = DataInputStream(connection.errorStream)
+        }
+
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val output: String = reader.readLine()
+        binding.responseBody.text = output
+
+        binding.responseCode.text = connection.responseCode.toString()
+        connection.disconnect();
+
     }
 }
 
